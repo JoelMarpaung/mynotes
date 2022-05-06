45d89d758017c1d276d57386808dff54ce180f67
Joel Marpaung <myitlife.jm@gmail.com>
2022-05-07 00:10:59 +0700

    implement auth service to login, register, and etc.

--- lib/services/auth/firebase_auth_provider.dart
+++ lib/services/auth/firebase_auth_provider.dart
@@ -1,8 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/services/auth/auth_user.dart';
 import 'package:mynotes/services/auth/auth_provider.dart';
 import 'package:mynotes/services/auth/auth_exceptions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:mynotes/utilities/show_error_dialog.dart';
 
 class FirebaseAuthProvider implements AuthProvider {
   @override
@@ -89,5 +90,12 @@ class FirebaseAuthProvider implements AuthProvider {
     } else {
       throw UserNotLoggedInAuthException();
     }
+  }
+
+  @override
+  Future<void> initialize() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
   }
 }
